c30c1b65f220906d69c377ba37ea89995b7ff9d8
frnode <gp.corentin@gmail.com>
2020-04-10 00:41:56 +0200

    Implemented restore command

--- py_wp_backup/py_wp_backup.py
+++ py_wp_backup/py_wp_backup.py
@@ -24,6 +24,9 @@ import ssl
 import subprocess
 import sys
 import tarfile
+import tempfile
+from pprint import pprint
+
 import gnupg as gnupg_
 import regex as regex
 from wpconfigr import WpConfigFile
@@ -88,37 +91,46 @@ def cli(ctx, debug, archive_retention):
     click.echo('Retention of backups for ' + str(archive_retention) + ' days')
     ctx.obj['archive-retention'] = archive_retention
     ctx.obj['gpg_use'] = False
+    ctx.obj['gpg_private_use'] = False
     ctx.obj['transfer_use'] = False
-    ctx.obj['wp_use'] = False
+    ctx.obj['backup_use'] = False
+    ctx.obj['restore_use'] = False
 
-    required_wp_cmd = 'backup'
-    required_wp_cmd_backup = ['--wp', '--sql']
-    required_wp_cmd_options = ['--wp-dir', '-wd', '--archive-dir', '-a']
-
-    sys_argv_set = set(sys.argv)
-    required_wp_cmd_options_set = set(required_wp_cmd_options)
-    required_wp_cmd_backup_set = set(required_wp_cmd_backup)
-
-    if (len(sys_argv_set.intersection(required_wp_cmd_backup_set)) > 0) and (required_wp_cmd in sys.argv) \
-            and (len(sys_argv_set.intersection(required_wp_cmd_options_set)) == 2):
-        ctx.obj['wp_use'] = True
-    else:
-        click.echo('The "' + required_wp_cmd + '" command is required and options: ' +
-                   ', '.join(required_wp_cmd_options) + ' and and at least one: ' + ', '.join(required_wp_cmd_backup),
-                   err=True)
-        exit(2)
+    # required_wp_cmd = 'backup'
+    # required_wp_cmd_backup = ['--wp', '--sql']
+    # required_wp_cmd_options = ['--wp-dir', '-wd', '--archive-dir', '-a']
+    #
+    # sys_argv_set = set(sys.argv)
+    # required_wp_cmd_options_set = set(required_wp_cmd_options)
+    # required_wp_cmd_backup_set = set(required_wp_cmd_backup)
+    #
+    # if (len(sys_argv_set.intersection(required_wp_cmd_backup_set)) > 0) and (required_wp_cmd in sys.argv) \
+    #        and (len(sys_argv_set.intersection(required_wp_cmd_options_set)) == 2):
+    #    ctx.obj['backup_use'] = True
+    # else:
+    #    click.echo('The "' + required_wp_cmd + '" command is required and options: ' +
+    #               ', '.join(required_wp_cmd_options) + ' and and at least one: ' + ', '.join(required_wp_cmd_backup),
+    #               err=True)
+    #    exit(2)
 
 
 @cli.command()
-@click.option('--gpg-key-file', '-gkf', cls=Mutex, not_required_if=['gpg_key_server', 'gpg_key_id'])
-@click.option('--gpg-key-server', '-gks', type=str, default='keyserver.ubuntu.com')
-@click.option('--gpg-key-id', '-gki', required=True)
+@click.option('--key-file', '-kf', default=None, cls=Mutex, not_required_if=['key_server', 'key_id'])
+@click.option('--key-server', '-ks', type=str, default='keyserver.ubuntu.com')
+@click.option('--key-id', '-ki', type=str, default=None, cls=Mutex,
+              not_required_if=['private_key_file', 'private_key_file_remove'])
+@click.option('--private-key-file', '-pkf', type=click.Path(exists=True, readable=True, file_okay=True),
+              default=None, cls=Mutex, not_required_if=['key_server', 'key_id', 'key_file'])
+@click.option('--private-key-pass', '-pkp', cls=Mutex,
+              not_required_if=['key_server', 'key_id', 'key_file'], prompt=True, hide_input=True)
+@click.option('--private-key-file-remove', '-pkf', default=True, required=False, cls=Mutex,
+              not_required_if=['key_server', 'key_id', 'key_file'])
 @click.pass_context
-def gpg(ctx, gpg_key_server, gpg_key_id, gpg_key_file):
+def gpg(ctx, key_server, key_id, key_file, private_key_file, private_key_file_remove, private_key_pass):
     # Todo: Need cleanup, use try ?
-    if not ctx.obj['wp_use']:
-        click.echo('To use this command, the "backup" command is required.', err=True)
-        exit(2)
+    # if not (ctx.obj['backup_use'] or ctx.obj['restore_use']):
+    #    click.echo('To use this command, the "backup" or "restore" command is required.', err=True)
+    #    exit(2)
 
     ctx.obj['gpg_use'] = True
     click.echo("GPG initialization...")
@@ -127,25 +139,46 @@ def gpg(ctx, gpg_key_server, gpg_key_id, gpg_key_file):
     # export
     ctx.obj['gpg'] = gpg
 
-    public_key_exist = gpg.list_keys(keys=gpg_key_id)
+    if key_id is not None:
+        public_key_exist = gpg.list_keys(keys=key_id)
 
-    if public_key_exist.fingerprints:
-        click.echo('The key: "' + gpg_key_id + '" already exists')
-    else:
-        if gpg_key_file is not None:
-            click.echo('Import of the key located in the file: ' + gpg_key_file)
-            gpg.import_keys(gpg_key_file.read())
-        elif (gpg_key_id is not None) and (gpg_key_file is None):
-            click.echo('Reception of the key: "' + gpg_key_id + '" on the "' + gpg_key_server + '" server...')
-            imported_key = gpg.recv_keys(gpg_key_server, gpg_key_id)
-            if imported_key.count is None:
+        if public_key_exist.fingerprints:
+            click.echo('The key: "' + key_id + '" already exists')
+        else:
+            if key_file is not None:
+                click.echo('Import of the key located in the file: "' + key_file + '"')
+
+                with open(key_file) as f:
+                    key_data = f.read()
+                imported_key = gpg.import_keys(key_data)
+
+            elif (key_id is not None) and (key_file is None):
+                click.echo('Reception of the key: "' + key_id + '" on the "' + key_server + '" server...')
+                imported_key = gpg.recv_keys(key_server, key_id)
+                if imported_key.count is None:
+                    click.echo('Error. GPG')
+                    exit(2)
+
+            click.echo('The key has been successfully imported. Fingerprint: ' + imported_key.fingerprints[0])
+
+        # export
+        ctx.obj['gpg_key_id'] = key_id
+
+    # import private key
+    if private_key_file is not None:
+        ctx.obj['gpg_private_use'] = True
+        ctx.obj['gpg_private_key_pass'] = private_key_pass
+        ctx.obj['gpg_private_key_file_remove'] = private_key_file_remove
+
+        if private_key_file is not None:
+            click.echo('Import of the private key located in the file: "' + private_key_file + '"')
+            with open(private_key_file) as f:
+                private_key_data = f.read()
+            imported_private_key = gpg.import_keys(private_key_data)
+            ctx.obj['gpg_private_key_fingerprints'] = imported_private_key.fingerprints[0]
+            if imported_private_key.count is None:
                 click.echo('Error. GPG')
                 exit(2)
-
-        click.echo('The key has been successfully imported. Fingerprint: ' + imported_key.fingerprints[0])
-
-    # export
-    ctx.obj['gpg_key_id'] = gpg_key_id
 
 
 @cli.command()
@@ -157,9 +190,9 @@ def gpg(ctx, gpg_key_server, gpg_key_id, gpg_key_file):
 @click.option('--remove-local', '-rml', type=bool, default=False, required=True)
 @click.pass_context
 def transfer(ctx, host, user, passwd, timeout, mode, remove_local):
-    if not ctx.obj['wp_use']:
-        click.echo('To use this command, the "backup" command is required.', err=True)
-        exit(2)
+    # if not ctx.obj['backup_use']:
+    #    click.echo('To use this command, the "backup" command is required.', err=True)
+    #    exit(2)
 
     ctx.obj['transfer_use'] = True
 
@@ -185,7 +218,7 @@ def transfer(ctx, host, user, passwd, timeout, mode, remove_local):
               required=True)
 @click.pass_context
 def backup(ctx, wp, sql, wp_dir, archive_dir):
-    ctx.obj['wp_use'] = True
+    ctx.obj['backup_use'] = True
 
     if (wp is False) and (sql is False):
         click.echo('Please know at least one command for backup: "--wp" or "--sql" to use the backup command.')
@@ -261,7 +294,7 @@ def backup(ctx, wp, sql, wp_dir, archive_dir):
                    mysql_pw=wp_config.get('DB_PASSWORD'), database=wp_config.get('DB_NAME'), out_file=SQL_PATH_FILENAME)
 
         # gzip
-        sql_backup_file = tar([SQL_PATH_FILENAME], SQL_UNCRYPTED_PATH_FILENAME)
+        sql_backup_file = tar([SQL_PATH_FILENAME], SQL_UNCRYPTED_PATH_FILENAME, accname=SQL_UNCRYPTED_FILENAME + '.sql')
 
         # encrypt
         if ctx.obj['gpg_use']:
@@ -277,12 +310,149 @@ def backup(ctx, wp, sql, wp_dir, archive_dir):
                               remove_local_file=ctx.obj['transfer_remove_local'], close=True)
 
 
-def tar(src, out, mode='x:gz'):
+@cli.command()
+@click.option('--wp-archive', '-wa', default=None, type=click.Path(exists=True, writable=False, file_okay=True),
+              cls=Mutex,
+              not_required_if=['wp_dir'])
+@click.option('--sql-archive', '-sa', default=None, type=click.Path(exists=True, writable=False, file_okay=True))
+@click.option('--wp-dir', '-wd', default=None, required=True)
+@click.option('--sql-database', '-sd', default=None)
+@click.pass_context
+def restore(ctx, wp_archive, sql_archive, wp_dir, sql_database):
+    ctx.obj['restore_use'] = True
+
+    if (wp_archive is None) and (sql_archive is None):
+        click.echo(
+            'Please know at least one command for backup: "--wp-archive" or "--sql-archive" to use the restore command.', err=True)
+
+    if (sql_archive is not None) and (wp_dir is None):
+        click.echo('To use SQL restore please specify the option "--wp-dir"', err=True)
+
+    if ctx.obj['transfer_use']:
+        # get file in ftps
+        pass
+
+    if wp_archive is not None:
+        if ctx.obj['transfer_use']:
+            # future implementation
+            pass
+        else:
+            # decrypt
+            if ctx.obj['gpg_use']:
+                stream = open(wp_archive, "rb")
+
+                try:
+                    tmp_file = tempfile.NamedTemporaryFile(delete=True, suffix=".tar.gz")
+                except:
+                    click.echo('Unable to create temporary file', err=True)
+                    exit(2)
+                else:
+                    click.echo('Temporary file created: "' + tmp_file.name + '"')
+
+                click.echo('Attempting to decrypt the file: ' + wp_archive)
+
+                try:
+                    ctx.obj['gpg'].decrypt_file(stream, passphrase=ctx.obj['gpg_private_key_pass'],
+                                                output=tmp_file.name)
+                    tar_decompress(tmp_file.name, wp_dir)
+                except:
+                    click.echo('Unable to decrypt the file', err=True)
+                    exit(2)
+                else:
+                    click.echo('File successfully decrypted: ' + wp_archive)
+                finally:
+                    tmp_file.close()
+                    stream.close()
+
+                if ctx.obj['gpg_private_key_file_remove']:
+                    ctx.obj['gpg'].delete_keys(ctx.obj['gpg_private_key_fingerprints'], secret=True,
+                                               passphrase=ctx.obj['gpg_private_key_pass'])
+                    click.echo('Removed GPG private key: ' + ctx.obj['gpg_private_key_fingerprints'])
+            else:
+                tar_decompress(wp_archive, wp_dir)
+
+        click.echo('WordPress archive restored to location: "' + wp_dir + '"')
+
+    if sql_archive is not None:
+
+        if ctx.obj['gpg_use']:
+            sql_stream = open(sql_archive, "rb")
+
+            try:
+                sql_compress_tmp_file = tempfile.NamedTemporaryFile(delete=True, suffix=".tar.gz")
+            except:
+                click.echo('Unable to create temporary file', err=True)
+                exit(2)
+            else:
+                click.echo('Temporary file created: "' + sql_compress_tmp_file.name + '"')
+
+            click.echo('Attempting to decrypt the file: ' + sql_archive)
+
+            try:
+                ctx.obj['gpg'].decrypt_file(sql_stream, passphrase=ctx.obj['gpg_private_key_pass'],
+                                            output=sql_compress_tmp_file.name)
+
+                sql_compress_path_file = sql_compress_tmp_file.name
+            except:
+                click.echo('Unable to decrypt the file', err=True)
+                exit(2)
+            else:
+                click.echo('File successfully decrypted: ' + sql_archive)
+            finally:
+                sql_stream.close()
+
+            if ctx.obj['gpg_private_key_file_remove']:
+                ctx.obj['gpg'].delete_keys(ctx.obj['gpg_private_key_fingerprints'], secret=True,
+                                           passphrase=ctx.obj['gpg_private_key_pass'])
+                click.echo('Removed GPG private key: ' + ctx.obj['gpg_private_key_fingerprints'])
+        else:
+            sql_compress_path_file = sql_archive
+
+        try:
+            sql_tmp_dir_file = tempfile.TemporaryDirectory()
+        except:
+            click.echo('Unable to create temporary directory', err=True)
+            exit(2)
+        else:
+            click.echo('Temporary directory created: "' + sql_tmp_dir_file.name + '"')
+
+        sql_tmp_file = os.path.basename(sql_archive.replace('.encrypted.tar.gz', ''))
+
+        sql_file = os.path.join(sql_tmp_dir_file.name, sql_tmp_file) + '.sql'
+
+        tar_decompress(sql_compress_path_file, sql_tmp_dir_file.name)
+
+        if ctx.obj['gpg_use']:
+            sql_compress_tmp_file.close()
+
+        # get wp-config params
+        wp_config = WpConfigFile(os.path.join(wp_dir, 'wp-config.php'))
+
+        wp_db_host_port = parse_hostport(wp_config.get('DB_HOST'))
+        wp_db_host = wp_db_host_port[0]
+
+        if wp_db_host_port[1] is None:
+            wp_db_port = '3306'
+        else:
+            wp_db_port = wp_db_host_port[1]
+
+        if sql_database is None:
+            wp_db_name = wp_config.get('DB_NAME')
+        else:
+            wp_db_name = sql_database
+
+        sql_restore(hostname=wp_db_host, port=wp_db_port, mysql_user=wp_config.get('DB_USER'),
+                   mysql_pw=wp_config.get('DB_PASSWORD'), database=wp_db_name, file=sql_file)
+
+        #sql_tmp_file.close()
+
+
+def tar(src, out, mode='x:gz', accname=None):
     click.echo('Creating the compressed file...')
     try:
         tar = tarfile.open(out, mode)
         for file in src:
-            tar.add(file)
+            tar.add(file, accname)
             click.echo('File or folder "' + file + '" add to archive')
     except:
         click.echo('Error.', err=True)
@@ -291,6 +461,20 @@ def tar(src, out, mode='x:gz'):
 
     click.echo('Compression complete. File path: ' + tar.name)
     return tar.name
+
+
+def tar_decompress(file, to):
+    try:
+        click.echo('Decompressing the archive: "' + file + '"')
+        tf = tarfile.open(file)
+        tf.extractall(path=to)
+    except:
+        click.echo('Unable to decompress the archive', err=True)
+        exit(2)
+    else:
+        click.echo('File decompression performed: "' + file + '"')
+    finally:
+        tf.close()
 
 
 def encrypt_with_gpg(gpg, path, key, remove_file=False, output_path=None):
@@ -379,9 +563,11 @@ def ftp_transfer_file(ftp, filename, file_path, remove_local_file=False, close=T
 
 
 def sql_backup(hostname, port, mysql_user, mysql_pw, database, out_file):
+
     try:
         p = subprocess.Popen(
-            'mysqldump -u ' + mysql_user + ' --password=' + mysql_pw + ' -h ' + hostname + ' -P ' + port + ' -e --opt -c ' + database + ' > ' + out_file,
+            'mysqldump -u ' + mysql_user + ' --password=' + mysql_pw + ' -h ' + hostname + ' -P ' + port + ' -e --opt '
+            + '-c ' + database + ' > ' + out_file,
             shell=True)
         # Wait for completion
         p.communicate()
@@ -391,6 +577,22 @@ def sql_backup(hostname, port, mysql_user, mysql_pw, database, out_file):
         click.echo('SQL backup performed successfully. Database: ' + database)
     except:
         click.echo('Error during SQL Backup. Database: ' + database)
+        exit(2)
+
+
+def sql_restore(hostname, port, mysql_user, mysql_pw, database, file):
+    try:
+        p = subprocess.Popen(
+            'mysql -u ' + mysql_user + ' --password=' + mysql_pw + ' -h ' + hostname + ' -P ' + port + ' ' + database + ' < ' + file,
+            shell=True)
+        # Wait for completion
+        p.communicate()
+        # Check for errors
+        if p.returncode != 0:
+            raise
+        click.echo('SQL restore performed successfully. Database: ' + database)
+    except:
+        click.echo('Error during SQL restore. Database: ' + database)
         exit(2)
 
 
